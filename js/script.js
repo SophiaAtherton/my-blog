38bc50d918601a4bf7b2acd443be002eb7ae3762
SophiaAtherton <sophia.atherton@mds.ac.nz>
2025-05-26 10:27:37 +1200

    weeks 3 & 4

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//gsap.registerPlugin(ScrollTrigger);
+//const panels = gsap.utils.toArray(".panel");
+//panels.forEach((panel) => {
+  //ScrollTrigger.create({
+   // trigger: panel,               // Trigger the animation on the panel
+   // start: "top top",              // Start when the top of the panel hits the top of the viewport
+   // scrub: true,                   // Scrub for smooth scrolling
+   // pin: true,                     // Pin the panel while scrolling
+   // pinSpacing: false,             // Prevent additional spacing when pinned
+   // markers: true                  // Enable markers for debugging
+//  });
+//});
+//ScrollTrigger.create({
+ // trigger: ".content",            // Trigger effect when .content comes into view
+  //snap: {                         // Enable snapping behavior
+   // snapTo: 1 / (panels.length - 1), // Snap to each panel
+   // duration: 2,                  // Snapping duration
+  //},
+  //start: "top top",               // Trigger when the top of the content hits the top of the viewport
+  //end: "bottom bottom",           // End when the bottom of the content hits the bottom of the viewport
+  //markers: true                   // Enable markers for debugging
+//});
+
+
+//doesn't allow for main-content to scroll, something to do with vp heights?
